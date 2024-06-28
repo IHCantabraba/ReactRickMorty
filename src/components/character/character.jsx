bd0461955d568ec055228eb02f7ece77039f0101
IHCantabraba <inigocanta@gmail.com>
2024-06-28 20:17:15 +0200

    rotate img

--- src/components/character/character.jsx
+++ src/components/character/character.jsx
@@ -6,27 +6,14 @@ function Character({ character }) {
   return (
     <>
       <div className='container text-center p-5'>
-        <button
-          className='char-btn'
-          id={character.id}
-          onClick={() => {
-            setRotation(!rotate)
-          }}
-        ></button>
         <h4 key={character.name}>{character.name}</h4>
-        {rotate ? (
-          <img
-            src={character.image}
-            alt={character.name}
-            className='rotate-180 rounded-pill'
-          ></img>
-        ) : (
-          <img
-            src={character.image}
-            alt={character.name}
-            className='rounded-pill'
-          ></img>
-        )}
+        <img
+          src={character.image}
+          alt={character.name}
+          className={rotate ? 'rotate-180 rounded-pill' : 'rounded-pill'}
+          onClick={() => setRotation(!rotate)}
+        ></img>
+
         <p>{character.origin.name}</p>
       </div>
     </>
